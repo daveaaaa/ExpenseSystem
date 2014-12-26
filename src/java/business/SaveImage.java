7d5b0e8515e6d7691b12890fa9b1152410a928c1
david <david@david-netbook>
2014-12-26 18:36:18 +0000

    setup

---
+++ src/java/business/SaveImage.java
@@ -0,0 +1,16 @@
+package business;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author david
+ */
+public class SaveImage {
+    
+    public HttpServletRequest doAction(HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
